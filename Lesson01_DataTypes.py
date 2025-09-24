e6fb59ba24549eb9317b53dfbe0b8060b7bd4882
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-09-24 16:18:50 +0000

    Lesson01 DataTypes Script

---
+++ Lesson01_DataTypes.py
@@ -0,0 +1,38 @@
+# This is a comment
+#DATA TYPES
+
+# 1. Strings:
+print("Hello World!")
+print(" ")
+print("You are cool! \nThanks \n- A Cool Guy")
+print("Name: \tBilly \tAge: \t12 \tGrade: \t10")
+
+# 2. Integers:
+print(14)
+print(-14)
+print(12 + 12 - 10)
+
+# 3. Floats:
+print(14.0)
+print(-14.0)
+print(12.0 + 12.0 - 10.0)
+print(10.0 + 4) # results as a float
+
+# 4. Booleans:
+print(True) # 1
+print(False) # 0
+print(14 == 2)
+print(14 == 14)
+print(14 == 14.0)
+print(14 != 2)
+print(14 != 14)
+print("Hello!" == "Hello!")
+print("Hello!" == "hello!")
+print("Hello!" != "Hello!")
+print("Hello!" != "hello!")
+
+#How do we know the data type of something?
+print(type("Hello!"))
+print(type(14))
+print(type(14.0))
+print(type(True))

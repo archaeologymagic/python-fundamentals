a6d1222c06ef81f3c8751debe126a640c07bb260
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-09-25 17:27:35 +0000

    Lesson02 Variables Script

---
+++ Lesson02_Variables.py
@@ -0,0 +1,41 @@
+#CONCEPTS VARIABLES
+
+Taco = "Cool Food"
+
+password = "P@ssw0rd!"
+email = "password@password.com"
+
+print("Password: " + password)
+print("Email: " + email)
+
+temperature = 14.14
+print(type(temperature))
+
+enables = False
+is_complete = True
+
+print(is_complete)
+
+x = "Radia Perlmn"
+y = 34
+z = "Networking Engineer"
+RAP = x
+Age = y
+Job = z
+
+count = 10
+minus = -5
+count += minus
+print(count)
+
+x = 4
+y = "Hello"
+print(x, " & ", y)
+temp = x
+x = y
+y = temp
+print(x, " & ", y)
+
+is_raining = False
+is_raining = True
+print("Is it raining?: ", is_raining)

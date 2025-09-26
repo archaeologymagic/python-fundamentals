745b9c71d909bfea298dc145909f8e8313a611cf
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-09-26 18:01:24 +0000

    Lesson03 Math Script

---
+++ Lesson03_Math.py
@@ -0,0 +1,44 @@
+#KEY CONCEPTS: Math Operators: +, -, *, /, //, %, **
+
+add = 7 + 2
+# Addition
+print("Sum: ", add)
+
+subtract = 7 - 2
+# Subtraction
+print("Difference: ", subtract)
+
+multiply = 7 * 2
+# Multiplication
+print("Product: ", multiply)
+
+float_divide = 8 / 2
+# Ceiling Division | Rounds Up
+print("Float Quotient: ", float_divide)
+
+integer_divide = 8 // 3
+# Floor Division | Rounds Down
+print("Integer Quotient: ", integer_divide)
+
+modulus = 7 % 2
+# Returns the remainder of a division
+print("Remainder: ", modulus)
+
+exponent = 2 ** 3
+# Exponentiation | Power
+print("Exponent: ", exponent)
+
+Result1 = (2 + 3) * 4
+print("Result1: ", Result1)
+
+Result2 = 2 ** 3 * 4
+print("Result2: ", Result2)
+
+Result3 = 20 / 5 * 2
+print("Result3: ", Result3)
+
+Result4 = 10 - 2 + 3
+print("Result4: ", Result4)
+
+Result5 = 5 + 2 ** 3 * (4-1)
+print("Result5: ", Result5)

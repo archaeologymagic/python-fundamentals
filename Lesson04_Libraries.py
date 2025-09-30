4d6a00c6ae350d765873607b4d3b4fce0ac7977c
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-09-30 15:56:34 +0000

    Lesson04 Libraries Script /w import math example

---
+++ Lesson04_Libraries.py
@@ -0,0 +1,10 @@
+# Python libraries are repositories of code.
+#To import a library, type "import" followed by the library name.
+
+import math #Importing the math library
+print("\n-- Math Library --")
+print("Square Root: ", math.sqrt(14))
+print("Round Up: ", math.ceil(1.4))
+print("Round Down: ", math.floor(1.4))
+print("Power: ", math.pow(12, 12))
+print("Pi Value: ", math.pi)

6b425659bb9a379c507ff66b75f6b8127e6ef3ad
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-09-30 15:55:43 +0000

    Lesson03 Math Script /w Challanges

--- Lesson03_Math.py
+++ Lesson03_Math.py
@@ -42,3 +42,33 @@ print("Result4: ", Result4)
 
 Result5 = 5 + 2 ** 3 * (4-1)
 print("Result5: ", Result5)
+
+# Challanges
+
+Rectangle_Dimensions = (8, 5)
+Area = Rectangle_Dimensions[0] * Rectangle_Dimensions[1]
+print(Area)
+
+m = 3.14
+r = 7
+Circle_Calculation = m * r ** 2
+print(Circle_Calculation)
+
+Shopping_List = {
+    "Book": 12.99,
+    "Notebook": 3.50
+}
+
+calculation = (Shopping_List["Book"] * 3) * (Shopping_List["Notebook"] * 4)
+print(calculation)
+
+print(7.0 % 4.9)
+
+number = int(input("Enter a number to check if it is even or odd: "))
+checker = number % 2
+if checker == 0:
+    print("The number is even")
+elif checker == 1:
+    print("The number is odd")
+else:
+    print("Invalid input")

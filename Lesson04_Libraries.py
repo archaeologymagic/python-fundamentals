91d7233018faeedd75a3a3bdafcce47236e0d2af
archaeologymagic <115739030+archaeologymagic@users.noreply.github.com>
2025-10-05 18:30:11 +0000

    Lessons 4 & 5

--- Lesson04_Libraries.py
+++ Lesson04_Libraries.py
@@ -1,10 +1,33 @@
 # Python libraries are repositories of code.
-#To import a library, type "import" followed by the library name.
+# To import a library, type "import" followed by the library name.
 
-import math #Importing the math library
-print("\n-- Math Library --")
-print("Square Root: ", math.sqrt(14))
-print("Round Up: ", math.ceil(1.4))
-print("Round Down: ", math.floor(1.4))
-print("Power: ", math.pow(12, 12))
-print("Pi Value: ", math.pi)
+import math  # Importing the math library
+print("\n-- Math Library --")  # Print a section header
+print("Square Root: ", math.sqrt(14))  # Print the square root of 14
+print("Round Up: ", math.ceil(1.4))  # Print the ceiling of 1.4
+print("Round Down: ", math.floor(1.4))  # Print the floor of 1.4
+print("Power: ", math.pow(12, 12))  # Print 12 to the power of 12
+print("Pi Value: ", math.pi)  # Print the value of pi
+
+
+
+# Random
+
+import random
+random_int = random.randint(10000, 99999)
+random_index = random.randint(1, 5)
+random_int_table = []
+for i in range(5):
+    random_int_table.append(random_int)
+    random_int = random.randint(10000, 99999)
+    print(random_int_table)
+chosenseed = random_int_table[random_index]
+print("Chosen Seed:", chosenseed)
+
+while not (1 < chosenseed < 10):
+    random_div = random.randint(2, 5) 
+    random_mult = random.randint(1, 2)
+    chosenseed = (chosenseed / random_div) * random_mult
+
+chosenseed = math.floor(chosenseed)
+print("Final Key:", chosenseed)
---
+++ Lesson05_Input.py
@@ -0,0 +1,29 @@
+# print("\n--- User Input Documentation ---")
+# name = input("Enter your name: ")
+# print("Hello ", name)
+
+# age = int(input("Enter your age: "))
+# print("You are soon to be", age + 1)
+# print(type(age))
+
+# height = float(input("Enter your height in meters: "))
+# print("You are", height, "meters tall.")
+# print(type(height))
+
+# color = input("Enter your favorite color: ")
+# print("Your favorite color is", color)
+
+# number1 = int(input("Enter a number: "))
+# number2 = int(input("Enter another number: "))
+# sum = number1 + number2
+# print("The sum of the numbers is", sum)
+
+# diameter = float(input("Enter the diameter of the circle: "))
+# area = 3.14 * ((diameter / 2) ** 2)
+# print("The area is:", area)
+
+import random
+sides = int(input("Enter the number of sides: "))
+roll = random.randint(1, sides)
+print("You rolled a", roll)
+
